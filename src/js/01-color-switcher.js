79462e8af7100fbbcb7a6699409994cb06e8468c
Budankov Kostya <107200706+Budankov@users.noreply.github.com>
2022-11-28 12:46:40 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
-console.log('hello');
+const startBtn = document.querySelector('button[data-start]');
+const stoptBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+
+startBtn.addEventListener('click', changeColor);
+stoptBtn.addEventListener('click', stopInterval);
+
+let intervalId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeColor() {
+  intervalId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+    startBtn.disabled = true;
+    startBtn.setAttribute = 'disabled';
+  }, 1000);
+}
+
+function stopInterval() {
+  startBtn.disabled = false;
+  clearInterval(intervalId);
+}
